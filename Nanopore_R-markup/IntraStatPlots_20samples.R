bafb0ba1980f83386f9793f34dd946a2969e016a
SkabbiVML <skabbi@gmail.com>
2020-12-16 14:33:52 +0100

    Adding the files to NDMA
    Had all the relative files on dropbox, just copiet it over here. I should be working from here if I make any changes to the code or files from now on

---
+++ Nanopore_R-markup/IntraStatPlots_20samples.R
@@ -0,0 +1,74 @@
+# Script to produce Figures 1d and 1e
+
+setwd("D:/IntraopR")
+
+library(ggsci)
+
+Seq_stats <- read.csv("SeqStats.csv", header = T)
+
+Seq_stats <- mutate(Seq_stats, sum_seq = Mean_read_length * Read_num)
+
+############## CpGs vs Read numbers ########################
+library(ggplot2)
+
+P1 <- ggplot(Seq_stats, aes(x = log10(CpG_sites), y = log10(sum_seq), fill = Minutes)) 
+
+P2 <- P1 + geom_smooth(mapping = aes(linetype = "r1"),
+                 method = "loess",
+                 formula = y ~ x, 
+                 se = TRUE,
+                 color = "black",
+                 show.legend = F)
+
+Q1 <- P2 + geom_point(size=5, stroke = 1, shape = 21, alpha = 0.8) +
+#ggtitle("Coverage (bp) as function of total CpG sites") +
+  theme_bw(base_size=15) +
+  scale_fill_gradientn(colors=blues9,name="Sequencing\ntime (min)") +
+    theme(legend.position="none")+
+  scale_y_continuous(name="Total nucleotides analyzed (log10)") +
+  scale_x_continuous(name="Total CpG sites analyzed (log10)")
+Q1
+
+###########################################
+
+################# Read Number vers Error rate #################
+P5 <- ggplot(Seq_stats, aes(x = log10(CpG_sites), y = Subclass.OOB.error, fill = Minutes))
+
+P6 <- P5 + geom_smooth(mapping = aes(linetype = "r1"),
+                       method = "loess",
+                       formula = y ~ x, 
+                       se = TRUE,
+                       color = "black",
+                       show.legend = F)
+
+Q3 <- P6 + geom_point(size=5, stroke = 1, shape = 21, alpha = 0.8) +
+  #ggtitle("Out-of-Bag error rate as a function of CpG sites")+
+  theme_bw(base_size=15) +
+  scale_fill_gradientn(colors=blues9,name="Sequencing\ntime (min)") +
+  theme(legend.position=c(.75, .75))+
+  theme(legend.title = element_text(colour="black", size=16, face="bold"))+
+  scale_x_continuous(name="Total CpG sites analyzed (log10)") +
+  scale_y_continuous(name="Out-of-Bag error rate")
+
+Q3
+#######################################
+library(tidyr)
+MCF_Seq_stats <- Seq_stats %>% filter(Minutes==30) %>% select(Patient, CpG_sites, Subclass.OOB.error, MCF.OOB.error) %>%
+                               gather("MCF.OOB.error", "Subclass.OOB.error", key = "Classifier", value = "OOB_Error")
+
+############# MCF versus subclass error #####
+p7 <- ggplot(MCF_Seq_stats, aes(x=Classifier, y = OOB_Error, fill = CpG_sites))
+
+Q6 <- p7 + 
+  geom_jitter(size=8, width = 0.05, height = 0, alpha = 0.8, shape = 21, stroke = 1)+
+  scale_fill_viridis_c(direction = -1) +
+  scale_y_continuous(limits = c(0,10), name="Out-of-Bag error rate")+
+  #theme(legend.position=c(.80, .25))+
+  theme(legend.title = element_text(colour="black", size=16, face="bold"))+
+  scale_x_discrete(name="Classifier", labels = c("MCF","Subclass")) +
+  theme_bw(15) 
+Q6
+
+########################
+library(gridExtra)
+grid.arrange(Q1,  Q3, Q6, nrow = 1)
---
+++ Nanopore_R-markup/Sankey_105samples.R
@@ -0,0 +1,85 @@
+# This script generates figure 1b, a sankey of all samples, how they were diagnosed by pathology and NDMA and if they are concordant
+
+setwd("~/IntraopR")
+
+
+
+library(dplyr)
+library(tidyr)
+library(tibble)
+
+# Data is imported and turned into a tibble so all dplyr functions function properly
+FULL <- read.csv("FULL_Nanopore_FINAL.csv", header=T)
+FULL <- as_tibble(FULL)
+
+# # If subsetting the data
+# Cohort <- FULL %>% filter(Cohort == "Pediatric") # use this to make any subset for plotting
+# Cohort <- FULL %>% filter(Category == "Intraop") # use this to make any subset for plotting
+
+# select the relevant columns for creating the flow chart. All IDs here should be represented in the colormap below 
+links <- select(FULL, "Simp_Patho_diagnosis","Nanopore_Capper", "Concordance")
+#links <- select_(Cohort, "Simp_Patho_diagnosis","Nanopore_Capper", "Concordance")
+
+#
+links <- links %>% unite(First_key, Simp_Patho_diagnosis, Nanopore_Capper, sep="->", remove = F) %>%
+  unite(Second_key, Nanopore_Capper, Concordance, sep="->", remove = F) %>%
+  select(First_key, Second_key, Concordance) %>% 
+  gather(Keys, Concordance) %>%
+  select(Concordance) %>% group_by(Concordance) %>% tally(sort = TRUE) %>%
+  separate(Concordance, c("Source","Target"), sep="->")
+
+#change the column names to match the Sankey inbut
+colnames(links)<-c("Source", "Target", "value")
+
+# For some reason the coloring fails if there is a space in the name
+links$Source <- gsub(" ", "_", links$Source)
+
+# Create a node data frame: it lists all entities involved in the Sankey
+nodes <- data.frame(
+  name=c(as.character(links$Source), 
+         as.character(links$Target)) %>% unique()
+)
+
+# With networkD3, connection must be provided using id, not using real name like in the links dataframe. Need to reformat it.
+links$IDsource <- match(links$Source, nodes$name)-1 
+links$IDtarget <- match(links$Target, nodes$name)-1
+
+# Load the color map from Capper et al
+Colormap <- read.csv("ColorMapFull.csv", header = T)
+colnames(Colormap)<-c("name", "colorCode")
+Colormap$name <- gsub(" ", "_", Colormap$name)
+
+FindColors <- left_join(nodes, Colormap, by = "name")
+
+color_scale <- data.frame(
+  range = FindColors$colorCode,
+  domain = FindColors$name,
+  nodes = nodes,
+  stringsAsFactors = FALSE
+)
+
+library(networkD3)
+# Make the network
+p <- sankeyNetwork(Links = links, Nodes = nodes,
+                   Source = "IDsource", Target = "IDtarget",
+                   Value = "value", 
+                   NodeID = "name",
+                   fontSize = 18,
+                   fontFamily = "sans-serif",
+                   nodeWidth = 15,
+                   nodePadding = 15,
+                   sinksRight=T,
+                   margin = c(top=50,right=0, left=0,bottom=50),
+                   iterations = 100,
+                   colourScale = JS(
+                     sprintf(
+                       'd3.scaleOrdinal()  
+  .domain(%s)
+  .range(%s)
+',
+                       jsonlite::toJSON(color_scale$domain),
+                       jsonlite::toJSON(color_scale$range)
+                     )
+                   )
+)
+p
